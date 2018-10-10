5043f172f1666159b23e5c6ee9c5be6543c7cbc4
padwekar <lifesavi@gmail.com>
2018-10-10 15:21:28 +0530

    Date Method added.

--- src/main/kotlin/TestServer.kt
+++ src/main/kotlin/TestServer.kt
@@ -7,7 +7,6 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import java.time.LocalDate
 
 fun main(args: Array<String>) {
         embeddedServer(Netty, 8080,watchPaths = listOf("Server/src/main/kotlin"),module = Application::calls).start(wait = true)
@@ -21,12 +20,8 @@ fun Application.calls(){
             call.respondText("Hello Healer", ContentType.Text.Html)
         }
 
-        get("/name") {
-            call.respondText("Saurabh", ContentType.Application.Json)
-        }
-
-        get("/date") {
-            call.respondText(LocalDate.now().toString(), ContentType.Text.Html)
+        get("/same") {
+            call.respondText("Same same", ContentType.Application.Json)
         }
     }
 }
