17e48869777123f42eaddc39e2bf0134388179d2
padwekar <lifesavi@gmail.com>
2018-10-10 16:33:05 +0530

    Memory Optimisation and Imports.

--- src/main/kotlin/TestServer.kt
+++ src/main/kotlin/TestServer.kt
@@ -1,33 +1,23 @@
 package com.healer.server
 
-import java.lang.reflect.Array.get
-import io.ktor.application.*
-import io.ktor.http.*
-import io.ktor.response.*
-import io.ktor.routing.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
-import java.time.LocalDate
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.routing
+import io.ktor.routing.get
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 
 fun main(args: Array<String>) {
         embeddedServer(Netty, 8080,module = Application::calls).start(wait = true)
 }
-
 
 fun Application.calls(){
 
     routing {
         get("/") {
             call.respondText("Hello Healer", ContentType.Text.Html)
-        }
-
-        get("/name") {
-            call.respondText("Saurabh", ContentType.Application.Json)
-        }
-
-        get("/date") {
-
-            call.respondText(LocalDate.now().toString(), ContentType.Application.Json)
         }
     }
 }
